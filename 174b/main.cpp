9a67c46d1ed56d2550a095cf61b3aaec6ca93c92
George <xxuam@connect.ust.hk>
2018-03-15 21:16:34 -0700

    Initial Commit

---
+++ 174b/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  174b
+//
+//  Created by Xiaofeng Xu on 15/03/2018.
+//  Copyright © 2018 Xiaofeng Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
